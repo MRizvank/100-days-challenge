37512120b028325545eea3e0bd4592f7300ed5be
Rizvan <rizvankurungattil@gmail.com>
2022-12-23 22:44:02 +0530

    array reduce

---
+++ Day-7/arrayreduce.js
@@ -0,0 +1,12 @@
+let users = [
+    {id: 1, name: "John"},
+    {id: 2, name: "Pete"},
+    {id: 3, name: "Mary"}
+  ];
+  
+  let newArr = users.reduce((acc,current)=>{
+    return{
+    currentcurrent.name:current[current.id]
+    }
+    
+  });
